279e2c7d2200ad32b52b5f7de5267e526a67cd19
jarretjefferson <jayesper47@gmail.com>
2023-08-05 09:34:40 -0700

    did hw assignment for day 2

--- functions_practice.py
+++ functions_practice.py
@@ -1,31 +1,16 @@
 def hello():
-    print("This is python")
+    print("Welcome to python")
 
 hello()
 
-def pack(food1, food2, food3):
-    snacks = [food1, food2, food3]
-    return snacks
+def pack(store1,store2,store3):
+    result = ["Chips", "Soda", "Candy"]
+    return result
 
-food_item1 = "Chips"
-food_item2 = "Fruit"
-food_item3 = "Yogurt"
-
-result = pack(food_item1, food_item2, food_item3)
-print(result)
+pack()
 
 
 
-def eat_lunch(food_list):
-    if not food_list:
-        print("My lunchbox is empty!")
-    else:
-        print("First I eat", food_list[0])
-        for food in food_list[1:]:
-            print("Next I eat", food)
-
-food_items = ["Sandwich", "Chips", "Fruit", "Yogurt"]
-eat_lunch(food_items)
 
 
 
